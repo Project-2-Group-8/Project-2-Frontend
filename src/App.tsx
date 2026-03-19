8681ff8cf01a19416b060be7d93e66ab2def2242
Brandon Algarra <balgarra@csumb.edu>
2026-03-18 20:47:19 -0700

    Added browser router to main.tsx to navigate between pages, added a couple routes from app.tsx, added page to edit hike log with id#

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HikeList from './components/HikeList'
 import HikeForm from './components/HikeForm'
+import EditHikePage from './pages/EditHikePage'
 
-function App() {
+function Dashboard() {
   const [hikes, setHikes] = useState([])
 
   // Function to fetch all hikes
@@ -20,7 +22,7 @@ function App() {
   }, [])
 
   // This function is passed to the Form; it runs after a successful POST
-  const handleHikeAdded = (newHike) => {
+  const handleHikeAdded = () => {
     fetchHikes(); 
 
   }
@@ -42,6 +44,16 @@ function App() {
         </section>
       </main>
     </div>
+  )
+}
+
+// Main App component with routing
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/edit/:id" element={<EditHikePage />} />
+    </Routes>
   )
 }
 
